8fe07a030117997fa05bb8d1fc5bb0634953dcc2
Tadej Vengust <vengust.tadej@gmail.com>
2020-12-22 09:28:48 +0100

    Change creating existing tokens to creation with signature.

--- src/contracts/tokens/biosample-permission-token.sol
+++ src/contracts/tokens/biosample-permission-token.sol
@@ -46,7 +46,7 @@ contract BiosamplePermissionToken is
   {
     nftName = _name;
     nftSymbol = _symbol;
-    namespace = _namespace;
+    namespace = "io.genobank.test";
 
     // Mint existing tokens
     NFToken._mint(0x4d5dD2e41c226B63058dC1e972dDfD33415fE820, 0x000000000000000000000001633f5500a87c3dbb9c15f4d41ed5a33dacaf4184);
@@ -58,50 +58,108 @@ contract BiosamplePermissionToken is
     NFToken._mint(0xCef484DEB09B3dad49a51d3283C1e96809FAd2Bb, 0x000000000001000000000000633f5500a87c3dbb9c15f4d41ed5a33dacaf4184);
     NFTokenMetadata._setTokenUri(0x000000000001000000000000633f5500a87c3dbb9c15f4d41ed5a33dacaf4184, 'ACTIVE');
 
-    NFToken._mint(0xCef484DEB09B3dad49a51d3283C1e96809FAd2Bb, 0x00000000000100000000270fcef484deb09b3dad49a51d3283c1e96809fad2bb);
-    NFTokenMetadata._setTokenUri(0x00000000000100000000270fcef484deb09b3dad49a51d3283c1e96809fad2bb, 'ACTIVE');
+    createWithSignature(
+      0x00000000000100000000270fcef484deb09b3dad49a51d3283c1e96809fad2bb,
+      'ACTIVE',
+      0x000000000000000000000000000000000000000000000000000001762ec69ecf,
+      0x526497dfaca30bd36a4e1a5c910ae11f241d2bc1275821ffdc7478ecce3c1e15,
+      0x681104f5442818b04841aafa13774d2db1f37777b9680cf2a1a1d5c0c019eea9,
+      0x1b,
+      SignatureKind.eth_sign
+    );
 
     NFToken._mint(0x82c182381560A5d62241f49321162d2C84911184, 0x01d656c0e675000000000000633f5500a87c3dbb9c15f4d41ed5a33dacaf4184);
     NFTokenMetadata._setTokenUri(0x01d656c0e675000000000000633f5500a87c3dbb9c15f4d41ed5a33dacaf4184, 'ACTIVE');
 
-    NFToken._mint(0x82c182381560A5d62241f49321162d2C84911184, 0x01d656c0e67500000000270f82c182381560a5d62241f49321162d2c84911184);
-    NFTokenMetadata._setTokenUri(0x01d656c0e67500000000270f82c182381560a5d62241f49321162d2c84911184, 'ACTIVE');
+    createWithSignature(
+      0x01d656c0e67500000000270f82c182381560a5d62241f49321162d2c84911184,
+      'ACTIVE',
+      0x000000000000000000000000000000000000000000000000000001762ed1c68c,
+      0xd8f6bd8d302c42541f56d9483d312c3be987683f6eaf00dd034ef3bce276dbb9,
+      0x1e89dbc7ec056ba46204a350059ddc3478b25261b1685df16d6938c2284a7fdf,
+      0x1b,
+      SignatureKind.eth_sign
+    );
 
     NFToken._mint(0xE068f8f195Ac72F03748e0b652db802D69Bc1Ec0, 0x00e8990a4600000000000000633f5500a87c3dbb9c15f4d41ed5a33dacaf4184);
     NFTokenMetadata._setTokenUri(0x00e8990a4600000000000000633f5500a87c3dbb9c15f4d41ed5a33dacaf4184, 'ACTIVE');
 
-    NFToken._mint(0xE068f8f195Ac72F03748e0b652db802D69Bc1Ec0, 0x00e8990a4600000000000001e068f8f195ac72f03748e0b652db802d69bc1ec0);
-    NFTokenMetadata._setTokenUri(0x00e8990a4600000000000001e068f8f195ac72f03748e0b652db802d69bc1ec0, 'ACTIVE');
+    createWithSignature(
+      0x00e8990a4600000000000001e068f8f195ac72f03748e0b652db802d69bc1ec0,
+      'ACTIVE',
+      0x000000000000000000000000000000000000000000000000000001762eec4bc7,
+      0xacef912709b9a227ba69859454556c109c4cb1247e9d70c29a6aa76f20e7d5b4,
+      0x03259947085e4ab3d26856ebc23969db79a886f681dfa266cc6cdd77dc5d8c1c,
+      0x1b,
+      SignatureKind.eth_sign
+    );
 
     NFToken._mint(0x4312Ae73e398df66FBcC2FA82C235B9B14fd3307, 0x01d656c0e67e000000000000633f5500a87c3dbb9c15f4d41ed5a33dacaf4184);
     NFTokenMetadata._setTokenUri(0x01d656c0e67e000000000000633f5500a87c3dbb9c15f4d41ed5a33dacaf4184, 'ACTIVE');
 
-    NFToken._mint(0x4312Ae73e398df66FBcC2FA82C235B9B14fd3307, 0x01d656c0e67e0000000000014312ae73e398df66fbcc2fa82c235b9b14fd3307);
-    NFTokenMetadata._setTokenUri(0x01d656c0e67e0000000000014312ae73e398df66fbcc2fa82c235b9b14fd3307, 'ACTIVE');
+    createWithSignature(
+      0x01d656c0e67e0000000000014312ae73e398df66fbcc2fa82c235b9b14fd3307,
+      'ACTIVE',
+      0x000000000000000000000000000000000000000000000000000001763f25e2f2,
+      0xd7b2a19bcc9c1ba62c00a4ecbc5f6be383fc4827d9d215c7aef30950be6ed713,
+      0x79122d05ce6e605be95bfdd507c26dc97845855a5746eec5610a2802e167cf64,
+      0x1c,
+      SignatureKind.eth_sign
+    );
 
     NFToken._mint(0x4B235F7B5c1b21a55fA0A88f98cC9efD16bD6Bb3, 0x01d656c0e67c000000000000633f5500a87c3dbb9c15f4d41ed5a33dacaf4184);
     NFTokenMetadata._setTokenUri(0x01d656c0e67c000000000000633f5500a87c3dbb9c15f4d41ed5a33dacaf4184, 'ACTIVE');
 
-    NFToken._mint(0x4B235F7B5c1b21a55fA0A88f98cC9efD16bD6Bb3, 0x01d656c0e67c0000000000014b235f7b5c1b21a55fa0a88f98cc9efd16bd6bb3);
-    NFTokenMetadata._setTokenUri(0x01d656c0e67c0000000000014b235f7b5c1b21a55fa0a88f98cc9efd16bd6bb3, 'ACTIVE');
+    createWithSignature(
+      0x01d656c0e67c0000000000014b235f7b5c1b21a55fa0a88f98cc9efd16bd6bb3,
+      'ACTIVE',
+      0x0000000000000000000000000000000000000000000000000000017662db3639,
+      0x20a884704f4fcb632263c1443191995fe7613eaf655bac3c3735acbe9933c81f,
+      0x160b757da765ca8da758634a472139b0ca8a922023ce1bb9f541edb5f920efbe,
+      0x1b,
+      SignatureKind.eth_sign
+    );
 
     NFToken._mint(0x6ce3C7AB2Dd4F4d7D1cF51cD0DB62C4952Ef80ae, 0x01d656c0e688000000000000633f5500a87c3dbb9c15f4d41ed5a33dacaf4184);
     NFTokenMetadata._setTokenUri(0x01d656c0e688000000000000633f5500a87c3dbb9c15f4d41ed5a33dacaf4184, 'ACTIVE');
 
-    NFToken._mint(0x6ce3C7AB2Dd4F4d7D1cF51cD0DB62C4952Ef80ae, 0x01d656c0e6880000000000016ce3c7ab2dd4f4d7d1cf51cd0db62c4952ef80ae);
-    NFTokenMetadata._setTokenUri(0x01d656c0e6880000000000016ce3c7ab2dd4f4d7d1cf51cd0db62c4952ef80ae, 'ACTIVE');
+    createWithSignature(
+      0x01d656c0e6880000000000016ce3c7ab2dd4f4d7d1cf51cd0db62c4952ef80ae,
+      'ACTIVE',
+      0x000000000000000000000000000000000000000000000000000001766c91fa3d,
+      0x4ca7742b3532b6a87c972bf60124ac3a5ab6bf53fd1129612f7eabd6fc19a61f,
+      0x67a0008113ac6a14c6a2b2c1a7b39a53215844e5619df23b45fbf33c76bff46d,
+      0x1c,
+      SignatureKind.eth_sign
+    );
 
     NFToken._mint(0xA866502223C3b995fbaB48A9dF939bcaD90c2Aa6, 0x01d656c0e677000000000000633f5500a87c3dbb9c15f4d41ed5a33dacaf4184);
     NFTokenMetadata._setTokenUri(0x01d656c0e677000000000000633f5500a87c3dbb9c15f4d41ed5a33dacaf4184, 'ACTIVE');
 
-    NFToken._mint(0xA866502223C3b995fbaB48A9dF939bcaD90c2Aa6, 0x01d656c0e677000000000001a866502223c3b995fbab48a9df939bcad90c2aa6);
-    NFTokenMetadata._setTokenUri(0x01d656c0e677000000000001a866502223c3b995fbab48a9df939bcad90c2aa6, 'ACTIVE');
+    createWithSignature(
+      0x01d656c0e677000000000001a866502223c3b995fbab48a9df939bcad90c2aa6,
+      'ACTIVE',
+      0x000000000000000000000000000000000000000000000000000001767c5e576b,
+      0x5c528613bdfcddb37abfec9a3ffe608f19c259ffe80c18ddfb361315aae6f64f,
+      0x4f124411d5b70c9fc3d4c514fd90ef55573b1a496b821f2b6b8346ca443b597f,
+      0x1c,
+      SignatureKind.eth_sign
+    );
 
     NFToken._mint(0xbfE116a470F83465391c0c939db264Dea9777d12, 0x01d656c0e679000000000000633f5500a87c3dbb9c15f4d41ed5a33dacaf4184);
     NFTokenMetadata._setTokenUri(0x01d656c0e679000000000000633f5500a87c3dbb9c15f4d41ed5a33dacaf4184, 'ACTIVE');
 
-    NFToken._mint(0xbfE116a470F83465391c0c939db264Dea9777d12, 0x01d656c0e679000000000001bfe116a470f83465391c0c939db264dea9777d12);
-    NFTokenMetadata._setTokenUri(0x01d656c0e679000000000001bfe116a470f83465391c0c939db264dea9777d12, 'ACTIVE');
+    createWithSignature(
+      0x01d656c0e679000000000001bfe116a470f83465391c0c939db264dea9777d12,
+      'ACTIVE',
+      0x0000000000000000000000000000000000000000000000000000017681f60716,
+      0x92fcb8caf0445cf3efe3bd4400287cafb27414bb15c3b8ffe1ddc6b29fb4e009,
+      0x2cecdb2741cd0ebc6ad1022befaea44bbc2aab0fdcffdfe0a4ce8b0d88b6b67c,
+      0x1c,
+      SignatureKind.eth_sign
+    );
+
+    namespace = _namespace;
   }
 
   /// TODO: fix description
@@ -155,14 +213,14 @@ contract BiosamplePermissionToken is
    */
   function createWithSignature(
     uint256 _tokenId,
-    string calldata _permission,
+    string memory _permission,
     uint256 _seed,
     bytes32 _signatureR,
     bytes32 _signatureS,
     uint8 _signatureV,
     SignatureKind _signatureKind
   )
-    external
+    public
   {
     bytes32 _claim = getCreateClaim(_tokenId, _seed);
     require(
@@ -316,4 +374,4 @@ contract BiosamplePermissionToken is
     );
   }
 
-}
+} 
